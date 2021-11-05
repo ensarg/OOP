fff3d59f3d387bdaed62731e7738567e64173960
ensargul <40627914+ensargul@users.noreply.github.com>
2021-11-05 14:16:54 +0300

    config files

--- adapter2/src/edu/sehir/oop/Adapter2.java
+++ adapter2/src/edu/sehir/oop/Adapter2.java
@@ -4,7 +4,7 @@ public class Adapter2 {
 
     public static void main(String[] args) {
 
-        //new JTwoClassList();
-       new TwoList();
+        new JTwoClassList();
+       //new TwoList();
     }
 }
