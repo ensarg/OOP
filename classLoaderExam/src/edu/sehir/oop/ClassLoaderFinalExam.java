7535b06d7e2abc11e7eeb3a77ffcad65bd449398
ensarg <73120843+ensarg@users.noreply.github.com>
2021-11-08 16:28:09 +0300

    Add files via upload

---
+++ classLoaderExam/src/edu/sehir/oop/ClassLoaderFinalExam.java
@@ -0,0 +1,40 @@
+package edu.sehir.oop;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+
+public class ClassLoaderFinalExam extends ClassLoader{
+
+    public void testClassLoader(String classBinName){
+
+        try {
+
+            Object myClassObject = null;
+            ClassLoader classLoader = this.getClass().getClassLoader();
+
+            Class<?> loadedMyClass = classLoader.loadClass(classBinName);
+
+            System.out.println("Loaded class name: " + loadedMyClass.getName());
+
+            Constructor<?> constructor = loadedMyClass.getConstructor();
+            System.out.println("Constructor: " + constructor);
+
+            myClassObject = constructor.newInstance();
+
+            Method mm[] = loadedMyClass.getDeclaredMethods();
+
+            System.out.println(mm[0].invoke(myClassObject));
+            System.out.println(mm[1].invoke(myClassObject,3,5));
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
---
+++ classLoaderExam/src/edu/sehir/oop/ExamClass.java
@@ -0,0 +1,13 @@
+package edu.sehir.oop;
+
+public class ExamClass {
+
+    public ExamClass(){System.out.println("ExamClass object is created now");  }
+
+    public String sayMerhaba() {  return "Merhaba from the exam class !!";  }
+
+    public int calculate_example(int x, int y) {
+        return x*y;
+    }
+
+}
