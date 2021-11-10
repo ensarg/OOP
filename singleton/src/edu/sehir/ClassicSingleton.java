5caa5dc79b7c0229dc6530d4fbdea9155d7ac26d
ensarg <73120843+ensarg@users.noreply.github.com>
2021-11-10 11:23:38 +0300

    Add files via upload

---
+++ singleton/src/edu/sehir/ClassicSingleton.java
@@ -0,0 +1,24 @@
+package edu.sehir;
+
+/**
+ * Created by ensar on 17.10.2016.
+ */
+public class ClassicSingleton {
+    private static ClassicSingleton instance = null;
+    public static int instance_counter=0;
+    String e1;
+    private ClassicSingleton() {
+        e1 = new String("ensar Gul");
+        instance_counter++;
+    }
+
+    void writeName(){
+        System.out.println("my Name is "+e1 );
+    }
+    public static ClassicSingleton getInstance() {
+        if(instance == null) {
+            instance = new ClassicSingleton();
+        }
+        return instance;
+    }
+}
