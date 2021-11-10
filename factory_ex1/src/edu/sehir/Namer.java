5c62a62e50e58366048f4c958450ff169a813598
ensarg <73120843+ensarg@users.noreply.github.com>
2021-11-10 11:21:16 +0300

    Add files via upload

---
+++ factory_ex1/src/edu/sehir/Namer.java
@@ -0,0 +1,20 @@
+package edu.sehir;
+
+/**
+ * Created by ensar on 24.10.2016.
+ */
+public class Namer {
+
+        //base class extended by two child classes
+        protected String last;     //split name
+        protected String first;    //stored here
+
+        public String getFirst() {
+            return first;           //return first nmae
+        }
+        public String getLast() {
+            return last;            //return last name
+        }
+    }
+
+
