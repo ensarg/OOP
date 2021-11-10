313c2f1667bc1b3ac82fc84199ab03448399288a
ensarg <73120843+ensarg@users.noreply.github.com>
2021-11-10 11:21:35 +0300

    Add files via upload

---
+++ factoryMethod/src/edu/sehir/Circle.java
@@ -0,0 +1,11 @@
+package edu.sehir;
+
+/**
+ * Created by ensar on 17.10.2016.
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ factoryMethod/src/edu/sehir/Shape.java
@@ -0,0 +1,8 @@
+package edu.sehir;
+
+/**
+ * Created by ensar on 17.10.2016.
+ */
+public interface Shape {
+    void draw();
+}
