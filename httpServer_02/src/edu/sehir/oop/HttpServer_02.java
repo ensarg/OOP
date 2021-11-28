d0ef28d7a47a88cb352d5737cf2c2ef5027ac06e
ensargul <ensargul@sehir.edu.tr>
2021-11-28 13:16:22 +0300

    http server 2

---
+++ httpServer_02/src/edu/sehir/oop/HttpServer_02.java
@@ -0,0 +1,38 @@
+package edu.sehir.oop;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Date;
+
+// The tutorial can be found just here on the SSaurel's Blog :
+// https://www.ssaurel.com/blog/create-a-simple-http-web-server-in-java
+// Each Client Connection will be managed in a dedicated Thread
+public class HttpServer_02{
+    // port to listen connection
+    static final int PORT = 8000;
+    // verbose mode
+    static final boolean verbose = true;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverConnect = new ServerSocket(PORT);
+            System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+            // we listen until user halts server execution
+            while (true) {
+                JavaHTTPServer myServer = new JavaHTTPServer(serverConnect.accept());
+
+                if (verbose) {
+                    System.out.println("Connecton opened. (" + new Date() + ")");
+                }
+
+                // create dedicated thread to manage the client connection
+                Thread thread = new Thread(myServer);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Server Connection error : " + e.getMessage());
+        }
+    }
+}
