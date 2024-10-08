cf3d87c4adf3a2b0352bb14a67f65f5f1325cdfd
ensargul <40627914+ensargul@users.noreply.github.com>
2024-10-08 09:24:05 +0300

    simple 3

--- simple3/src/Simple3Main.java
+++ simple3/src/Simple3Main.java
@@ -3,7 +3,7 @@ public class Simple3Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello ensar ");
+        System.out.println("hello ensar. todays date is 2024-10-08.");
 
     }
 
