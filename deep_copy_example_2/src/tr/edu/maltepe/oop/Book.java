da50dff0e304f19320e96b8d6f6ecb2a5535d68f
ensargul <40627914+ensargul@users.noreply.github.com>
2025-05-14 20:13:14 +0300

    object copy update

---
+++ deep_copy_example_2/src/tr/edu/maltepe/oop/Book.java
@@ -0,0 +1,12 @@
+package tr.edu.maltepe.oop;
+
+public class Book {
+      String title;
+      String author;
+      Book(String title, String author) {
+         this.title = title;
+         this.author = author;
+      }
+}
+
+
---
+++ deep_copy_example_2/src/tr/edu/maltepe/oop/TestDeepCopy2.java
@@ -0,0 +1,30 @@
+package tr.edu.maltepe.oop;
+import java.util.ArrayList;
+
+public class TestDeepCopy2 {
+
+public static void main(String[] args) {
+   Book b1 = new Book("Book 1", "Author 1");
+   Book b2 = new Book("Book 2", "Author 2");
+   Book b3 = new Book("Book 3", "Author 3");
+   // Create a deep copy of the library
+   Library library = new Library(new ArrayList<Book>());
+   library.addBook(b1);
+   library.addBook(b2);
+   library.addBook(b3);
+   System.out.println("Original Library:");
+   library.printBooks();
+   Library libraryCopy = library.deepCopy();
+   System.out.println("Copied Library:");
+   libraryCopy.printBooks();
+
+   library.removeBook(b1);
+   //libraryCopy.addBook(new Book("Book 4", "Author 4"));
+   System.out.println("Original Library after modification:");
+   library.printBooks();
+   System.out.println("Copied Library:");
+   libraryCopy.printBooks();
+   // The copied library should not be affected by the changes in the original library
+
+}
+}
