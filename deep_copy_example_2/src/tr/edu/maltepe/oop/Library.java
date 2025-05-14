cdbb79184c3738f971e8f064e7e2b4115b3290ee
ensargul <40627914+ensargul@users.noreply.github.com>
2025-05-14 20:15:31 +0300

    object copy update

---
+++ deep_copy_example_2/src/tr/edu/maltepe/oop/Library.java
@@ -0,0 +1,31 @@
+package tr.edu.maltepe.oop;
+
+import java.util.ArrayList;
+
+public class Library {
+   ArrayList<Book> books;
+
+   Library(ArrayList<Book> books) {
+      this.books = books;
+   }
+   public void addBook(Book book) {
+      books.add(book);
+   }
+   public void removeBook(Book book) {
+      books.remove(book);
+   }
+   public void printBooks() {
+      for (Book book : books) {
+         System.out.println("Title: " + book.title + ", Author: " + book.author);
+      }
+   }
+   public Library deepCopy() {
+      ArrayList<Book> copiedBooks = new ArrayList<>();
+      for (Book book : books) {
+         copiedBooks.add(new Book(book.title, book.author));
+      }
+      return new Library(copiedBooks);
+   }
+
+
+}
