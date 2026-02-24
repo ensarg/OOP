e20af9c914a43c6beb488dd8faf0b06719eed29a
ensargul <40627914+ensargul@users.noreply.github.com>
2026-02-24 11:07:52 +0300

    asssignment template

---
+++ simple3/src/tr/edu/beykoz/oop/Simple3Main.java
@@ -0,0 +1,15 @@
+package tr.edu.beykoz.oop;
+
+public class Simple3Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("hello ensar. todays date is 2026-02-24.");
+        //Student student1 = new Student();
+        //student1.setName("Arda");
+        //System.out.println("student name is:"+student1.getName());
+    }
+
+}
+
+
---
+++ simple3/src/tr/edu/beykoz/oop/Student.java
@@ -0,0 +1,14 @@
+package tr.edu.beykoz.oop;
+
+public class Student {
+   private String name;
+   private int id;
+
+
+   String getName() {return name;}
+   int getId() {return id;}
+   void setName(String name) {this.name = name;}
+   void setId(int id) {this.id = id;}
+
+
+}
