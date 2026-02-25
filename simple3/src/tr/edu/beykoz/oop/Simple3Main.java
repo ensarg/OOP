3dcd7b1bb38c157950f1b247ac0a682320561dab
ensargul <40627914+ensargul@users.noreply.github.com>
2026-02-25 13:41:02 +0300

    change onr line

--- simple3/src/tr/edu/beykoz/oop/Simple3Main.java
+++ simple3/src/tr/edu/beykoz/oop/Simple3Main.java
@@ -5,9 +5,10 @@ public class Simple3Main {
     public static void main(String[] args) {
 
         //System.out.println("hello ensar. todays date is 2026-02-24.");
-        //Student student1 = new Student();
-        //student1.setName("Arda");
-        //System.out.println("student name is:"+student1.getName());
+        Student student1 = new Student();
+        student1.setName("Arda");
+
+        System.out.println("Student name is:"+student1.getName());
     }
 
 }
